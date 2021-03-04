d021543244f4462cc9512d2bf089231ba8dbfc6e
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-03 22:50:43 -0300

    05-Básico con R y RStudio

---
+++ basicofr.R
@@ -0,0 +1,35 @@
+#########################################
+##         OPERADOR MATEMÁTICO         ##
+2+5
+#########################################
+##         COMENTARIO                  ##
+# Demo Comment
+#########################################
+##         IMPRESIÓN POR CONSOLA       ##
+print("Hello World!!!")
+#########################################
+# Asignación de valores a variables
+# Más común
+x <- 2
+x
+
+y = 4
+y
+
+# Menos común 
+2 -> z
+z
+######################################################
+## ASIGNACIÓN DE UN CONJUNTO DE NÚMEROS A UNA VARIABLE
+######################################################
+c <-c(1,2,3,4,5)
+c1 <- 1:5
+
+#### SE ASIGNA EL MISMO CONJUNTO DE NÚMEROS A C2 Y C3
+c2 <- c3 <- 1:4
+
+######################################################
+# OPERACIONES CON DOS CONJUNTOS DE NÚMEROS
+######################################################
+c4 <- c2+c3
+c5 <- c2*c3
